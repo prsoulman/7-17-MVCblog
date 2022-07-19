36305de0906ad1adba833e50fc8192a9428342ff
prsoulman <60936175+prsoulman@users.noreply.github.com>
2022-07-19 17:04:38 -0500

    added routesd

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -22,4 +22,41 @@ router.post('/', async(req, res)=> {
     }
 
 })
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password']},
+        where: {
+          id: req.params.id
+        },
+        include: [
+            {
+              model: Post,
+              attributes: ['id', 'title', 'post_text']
+            },
+            {
+                model: Comments,
+                attributes: ['id', 'comment_text',],
+                include: {
+                  model: Post,
+                  attributes: ['title']
+                }
+            }
+          ]
+
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 module.exports= router
